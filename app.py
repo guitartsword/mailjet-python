46c68ba8dea983db09bbcac590160f5591a5ab34
Isaias Valle <guitartsword@gmai.com>
2018-02-09 22:15:21 -0600

    added cors to app

--- app.py
+++ app.py
@@ -2,9 +2,11 @@
 from flask import Flask
 from settings import PUBLIC_KEY, SECRET_KEY
 from api import api_bp
+from flask_cors import CORS
 
 APP = Flask(__name__)
 APP.register_blueprint(api_bp)
+CORS = CORS(APP)
 
 @APP.route('/')
 def hello():
