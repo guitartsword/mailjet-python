62a93b2e05b80bed291cec95b3627e1cdf3a74e5
Isaias Valle <guitartsword@gmai.com>
2018-02-11 22:20:55 -0600

    refactor app

--- app.py
+++ app.py
@@ -6,16 +6,19 @@ from flask_cors import CORS
 from settings import PUBLIC_KEY, SECRET_KEY, DATABASE_URL
 from api import api_bp, api_db
 
-APP = Flask(__name__)
-APP.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
-APP.register_blueprint(api_bp)
-CORS = CORS(APP)
-api_db.init_app(APP)
 
-with APP.app_context():
-    api_db.create_all()
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
+    app.register_blueprint(api_bp)
+    CORS(app)
+    api_db.init_app(app)
+    with app.app_context():
+        api_db.create_all()
 
-@APP.route('/')
-def hello():
-    """Simple Hello world."""
-    return 'Hello World!'
+    @app.route('/')
+    def hello():
+        """Simple Hello world."""
+        return 'Hello World!'
+    return app
+
