dead4600cb9ddc1918644c4d96ce7e951ce721de
Isaias Valle <guitartsword@gmai.com>
2018-03-18 12:16:37 -0600

    alembic test in heroku

---
+++ alembic/versions/6648c905e371_first_revision.py
@@ -0,0 +1,24 @@
+"""first revision
+
+Revision ID: 6648c905e371
+Revises: 
+Create Date: 2018-03-18 11:57:53.465011
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '6648c905e371'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
