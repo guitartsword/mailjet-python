0fea3e19a4cd15cb4a584790277da289352210ed
Isaias Valle <guitartsword@gmai.com>
2018-03-18 12:44:17 -0600

    added new revision

---
+++ alembic/versions/dc1e2820c8e7_change_type_of_columns.py
@@ -0,0 +1,28 @@
+"""change type of columns
+
+Revision ID: dc1e2820c8e7
+Revises: 6648c905e371
+Create Date: 2018-03-18 12:24:32.236262
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'dc1e2820c8e7'
+down_revision = '6648c905e371'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.alter_column('notification', sa.Column('html', sa.TEXT))
+    op.alter_column('notification', sa.Column('text', sa.TEXT))
+    pass
+
+
+def downgrade():
+    op.alter_column('notification', sa.Column('html', sa.String(100)))
+    op.alter_column('notification', sa.Column('text', sa.String(100)))
+    pass
